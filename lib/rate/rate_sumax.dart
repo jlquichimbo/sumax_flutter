e0e2fab648c441018779eb8b34464cc4341cbe9a
Jose Luis <joseluisquichimbo91@gmail.com>
2019-10-05 18:38:42 -0500

    first commit

---
+++ lib/rate/rate_sumax.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class RateSumax extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _RateSumax();
+  }
+
+}
+
+class _RateSumax extends State <RateSumax>{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    final containerRate = Container(
+
+    );
+    return containerRate;
+  }
+
+}
---
+++ lib/sumax.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class Sumax extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _Sumax();
+  }
+
+}
+
+
+class _Sumax extends State<Sumax>{
+  // Clase principal que contiene las diferentes pantallas de la app
+
+  //  Indice para acceder a cada elemento de la lista
+  int indexTap = 0;
+
+//  Lista de elementos a mostrar
+  final List<Widget> widgetsChildren = [];
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    final scaffoldSumax = Scaffold(
+      appBar: AppBar(),
+      body: ,
+
+    );
+
+    return scaffoldSumax;
+  }
+
+}
